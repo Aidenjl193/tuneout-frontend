f77d5bc3f030b645f56c3049cd509bfcb4a4419e
aidenjl193 <aidenjl193@gmail.com>
2019-09-18 15:20:03 +0100

    Custom audio player

--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -1,9 +1,81 @@
 import React from 'react'
 import './Player.css'
 
-export default props =>
-	<audio
-controls src={props.currentSong ? `https://tune-out.herokuapp.com/${props.currentSong.song_url}` : ""}
-onEnded={props.nextSong}
-ref={props.setPlayerRef}
-/>
+class Player extends React.Component {
+	state = {
+		song: {
+			duration: 0,
+			currentTime: 0
+		},
+		playing: false
+	}
+	
+	constructor(props) {
+		super(props);
+		this.playerRef = React.createRef();
+	}
+
+	componentDidMount() {
+		this.playerRef.current.oncanplay = () => {
+			const song = {...this.state.song}
+			song.duration = this.playerRef.current.duration;
+			this.setState({song: song});
+		};
+
+		this.playerRef.current.ontimeupdate = () => {
+			const song = {...this.state.song}
+			song.currentTime = this.playerRef.current.currentTime;
+			this.setState({song: song});
+		}
+	}
+
+	getFormattedTimeFromSeconds = (time) => {
+		const minutes = Math.floor(time / 60);
+		const seconds = Math.floor(time - minutes * 60);
+		return `${minutes}:${seconds < 10 ? 0 : ""}${seconds}`
+	}
+
+	togglePlay = () => {
+		if(this.state.playing) {
+			this.playerRef.current.pause();
+		} else {
+			this.playerRef.current.play();
+		}
+		this.setState({playing: !this.state.playing});
+	}
+
+	setTime = (e) => {
+		console.log("click");
+		this.playerRef.current.currentTime = this.state.song.duration * (e.nativeEvent.offsetX / e.currentTarget.offsetWidth);
+	}
+	
+	render() {
+		return (
+			<div>
+				<div id="player-bar">
+					<div id="progress-container" onClick={this.setTime}>
+						<div id="progress-background"/>
+						<div id="progress-bar" style={
+						{
+							width: `${100 * this.state.song.currentTime / this.state.song.duration}%`
+						}
+						}/>
+					</div>
+					<button onClick={this.togglePlay}>{this.state.playing ? "❚❚" : "►"}</button>
+					<p>
+						{
+							`${this.getFormattedTimeFromSeconds(this.state.song.currentTime)}/${this.getFormattedTimeFromSeconds(this.state.song.duration)}`
+						}
+					</p>
+				</div>
+				<audio
+				controls src={this.props.currentSong ? `https://tune-out.herokuapp.com/${this.props.currentSong.song_url}` : ""}
+				onEnded={this.props.nextSong}
+				ref={this.playerRef}
+				/>
+			</div>
+		)
+	}
+}
+
+export default Player;
---
+++ src/components/player/PlayerBar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Player.css'
+
+const PlayerBar = (props) => {
+	return (
+		<div id="player-bar"></div>
+	)
+}
